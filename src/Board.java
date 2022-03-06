7d380104b8af5787ff2beb77a9efb1f182245f67
Samhitha Tarra <samhitht@uci.edu>
2022-03-06 12:46:31 -0800

    majority of tictactoe implemention done

--- src/Board.java
+++ src/Board.java
@@ -1,34 +1,33 @@
-public class Board{
-
-    // row, col
+public class Board {
     Tile[][] boardArr;
     static final int ROW = 3;
     static final int COL = 3;
+    int playerXCounter = 0;
+    int playerOCounter = 0;
 
-    public Board(){
+    public Board() {
         boardArr = new Tile[ROW][COL];
     }
 
-    // can also be used to reset board?
-    public void createBoard(GameType gType){
+    public void createBoard(GameType gType) {
 
-        // BATTLESHIP        
-        // if(gType ==  GameType.BATTLESHIP){
-        //     for(int row = 0; row < boardArr.length; row++){
-        //         for(int col = 0; col < boardArr[0].length; col++){
-    
-        //             // boardArr[row][col].column = col;
-        //             // boardArr[row][col].row = row;
-        //             // boardArr[row][col].value = "0";                    
-        //         }
-        //     }
+        // BATTLESHIP
+        // if(gType == GameType.BATTLESHIP){
+        // for(int row = 0; row < boardArr.length; row++){
+        // for(int col = 0; col < boardArr[0].length; col++){
+
+        // // boardArr[row][col].column = col;
+        // // boardArr[row][col].row = row;
+        // // boardArr[row][col].value = "0";
+        // }
+        // }
         // }
 
         // TIC TAC TOE
-        if (gType ==  GameType.TICTACTOE){
-            for(int row = 0; row < boardArr.length; row++){
-                for(int col = 0; col < boardArr[0].length; col++){
-                    
+        if (gType == GameType.TICTACTOE) {
+            for (int row = 0; row < boardArr.length; row++) {
+                for (int col = 0; col < boardArr[0].length; col++) {
+
                     boardArr[row][col] = new Tile();
                     Tile tile = boardArr[row][col];
                     tile.column = col;
@@ -39,16 +38,47 @@ public class Board{
         }
     }
 
-    // via terminal
-    public void displayBoard(GameType gType){
-        if(gType ==  GameType.TICTACTOE){
+    // prints updated board
+    public void updateBoard(Tile move, GameType gType) {
+        boardArr[move.row][move.column].value = move.value;
+        displayBoard(GameType.TICTACTOE);
+
+        // TICTACTOE GAMETYPE CHECKING WIN EACH TIME
+        if (gType == GameType.TICTACTOE) {
+            checkWinTicTacToe(boardArr);
+            if (playerXCounter == 1) {
+                System.out.println("Player X got a win try to get one more!!");
+                displayBoard(gType);
+            }
+            if (playerOCounter == 1) {
+                System.out.println("Player O got a win try to get one more!!");
+                displayBoard(gType);
+            }
+            if (playerXCounter == 2) {
+                System.out.println("Player X got two wins they win!!");
+                System.exit(0);
+            }
+            if (playerOCounter == 2) {
+                System.out.println("Player O got two wins they win!!");
+                System.exit(0);
+            }
+        }
+
+        // BATTELESHIP GAMETYPE CHECKING WIN EACH TIME TO BE COMPLETED
+
+    }
+
+    public void displayBoard(GameType gType) {
+
+        // TICTACTOE GAMETYPE DISPLAY BOARD
+        if (gType == GameType.TICTACTOE) {
             String boardString = "";
 
-            for(int row = 0; row < boardArr.length; row++){
+            for (int row = 0; row < boardArr.length; row++) {
 
                 boardString += "\n";
 
-                for(int col = 0; col < boardArr[0].length; col++){
+                for (int col = 0; col < boardArr[0].length; col++) {
                     boardString += boardArr[row][col].value;
                     boardString += " ";
                 }
@@ -57,95 +87,126 @@ public class Board{
         }
     }
 
-    public boolean checkWin(Tile[][] boardArr){
-        Tile tile = new Tile();
+
+    // TICTACTOE GAMETYPE SPECIFIC FUNCTION TO CHECK WIN/LOGIC
+    public boolean checkWinTicTacToe(Tile[][] boardArr) {
         boolean win = false;
 
-        // check horizontals (row)
-        for(int row = 0; row < boardArr.length-1; row++){
+        // check horizontals
+        for (int row = 0; row < boardArr.length - 1; row++) {
 
             // check adjacents are same starting with 1st col (index 0)
-            if(boardArr[row][0] == boardArr[row][1]){
-                if(boardArr[row][1] == boardArr[row][2]){
-                    tile.value = boardArr[row][0].toString();
+            if (boardArr[row][1].value != "_") {
+                if (boardArr[row][0].value == boardArr[row][1].value) {
+                    if (boardArr[row][1].value == boardArr[row][2].value) {
 
-                    if(tile.value == "X"){
+                        if (boardArr[row][0].value == "X") {
+                            System.out.println("Player 1 wins!");
+                            playerXCounter++;
+                        } else {
+                            System.out.println("Player 2 wins!");
+                            playerOCounter++;
+                        }
                         win = true;
-                        System.out.println("Player 1 wins!");
-                    }
-                    else
-                    {
-                        win = true;
-                        System.out.println("Player 2 wins!");
+
+                        boardArr[row][0].value = "_";
+                        boardArr[row][1].value = "_";
+                        boardArr[row][2].value = "_";
+
+                        return win;
                     }
                 }
             }
         }
 
         // check verticals (cols)
-        for(int col = 0; col < boardArr.length-1; col++){
+        for (int col = 0; col < boardArr.length - 1; col++) {
             // check adjacents are same starting with 1st row (index 0)
-            if(boardArr[0][col] == boardArr[1][col]){
-                if(boardArr[1][col] == boardArr[2][col]){
-                    tile.value = boardArr[0][col].toString();
+            if (boardArr[1][col].value != "_") {
+                if (boardArr[0][col].value == boardArr[1][col].value) {
+                    if (boardArr[1][col].value == boardArr[2][col].value) {
 
-                    if(tile.value == "X"){
+                        if (boardArr[0][col].value == "X") {
+                            System.out.println("Player 1 wins!");
+                            playerXCounter++;
+                        } else {
+                            System.out.println("Player 2 wins!");
+                            playerOCounter++;
+                        }
                         win = true;
-                        System.out.println("Player 1 wins!");
-                    }
-                    else
-                    {
-                        win = true;
-                        System.out.println("Player 2 wins!");
+
+                        boardArr[0][col].value = "_";
+                        boardArr[1][col].value = "_";
+                        boardArr[2][col].value = "_";
+
+                        return win;
                     }
                 }
             }
         }
 
         // check diagonals
+        // for(int col = 0; col < boardArr.length-1; col++){
         // from top left to bottom right
-        if(boardArr[0][0] == boardArr[1][1]){
-            if(boardArr[1][1] == boardArr[2][2]){
-                tile.value = boardArr[0][0].toString();
+        if (boardArr[1][1].value != "_") {
+            if (boardArr[0][0].value == boardArr[1][1].value) {
+                if (boardArr[1][1].value == boardArr[2][2].value) {
 
-                if(tile.value == "X"){
-                    System.out.println("Player 1 wins!");
-                }
-                else
-                {
-                    System.out.println("Player 2 wins!");
+                    if (boardArr[0][0].value == "X") {
+                        System.out.println("Player 1 wins!");
+                        playerXCounter++;
+                    } else {
+                        System.out.println("Player 2 wins!");
+                        playerOCounter++;
+                    }
+                    win = true;
+
+                    boardArr[0][0].value = "_";
+                    boardArr[1][1].value = "_";
+                    boardArr[2][2].value = "_";
+
+                    return win;
                 }
             }
         }
 
         // from bottom left to top right
-        if(boardArr[2][0] == boardArr[1][1]){
-            if(boardArr[1][1] == boardArr[0][2]){
-                tile.value = boardArr[2][0].toString();
+        if (boardArr[1][1].value != "_") {
+            if (boardArr[2][0].value == boardArr[1][1].value) {
+                if (boardArr[1][1].value == boardArr[0][2].value) {
 
-                if(tile.value == "X"){
-                    System.out.println("Player 1 wins!");
-                }
-                else
-                {
-                    System.out.println("Player 2 wins!");
+                    if (boardArr[2][0].value == "X") {
+                        System.out.println("Player 1 wins!");
+                        playerXCounter++;
+                    } else {
+                        System.out.println("Player 2 wins!");
+                        playerOCounter++;
+                    }
+                    win = true;
+
+                    boardArr[2][0].value = "_";
+                    boardArr[1][1].value = "_";
+                    boardArr[0][2].value = "_";
+
+                    return win;
                 }
             }
         }
-
         return win;
+
     }
 
-    // check if tie game
-    public boolean fullBoard(Tile[][] boardArr){
+    // TICTACTOE GAMETYPE SPECIFIC FUNCTION TO CHECK FOR TIE
+    public boolean fullBoardTicTacToe(Tile[][] boardArr) {
 
-        for(int row = 0; row < boardArr.length; row++) {
-			for(int col = 0; col < boardArr[0].length; col++) {
-				if(boardArr[row][col].toString() == "_") {
-					return false;
-				}
-			}
-		}        
+        for (int row = 0; row < boardArr.length; row++) {
+            for (int col = 0; col < boardArr[0].length; col++) {
+                if (boardArr[row][col].value == "_") {
+
+                    return false;
+                }
+            }
+        }
         return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int turn = 0;   // 0 reps player 1's turn, 1 reps palyer 2's turn
 
         // User input
-        //Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
 
         // TIC TAC TOE LOGIC
@@ -17,19 +17,45 @@ public class Main {
         System.out.println();
         System.out.println("Player 1's game piece will be: X");
 		System.out.println("Player 2's game piece will be: O");
-
+        boardObj.createBoard(GameType.TICTACTOE);
         while(!gameFin){
-            boardObj.createBoard(GameType.TICTACTOE);
-            boardObj.displayBoard(GameType.TICTACTOE);
 
             // player 1's turn
             if(turn == 0){
                 System.out.println("Player 1's turn (X): ");
+                System.out.println("Enter the row you want to make your move in (start at 0)");
+                int rowInput = input.nextInt();
+                
+                System.out.println("Enter the column you want to make your move in (start at 0)");
+                int colInput = input.nextInt();
+                
+                Tile move1 = new Tile();
+                move1.value = "X";
+                move1.row = rowInput;
+                move1.column = colInput;
+
+                boardObj.updateBoard(move1, GameType.TICTACTOE);
+                turn = 1;
             }
             else{    // player 2's turn
                 System.out.println("Player 2's turn (O): ");
-            }
-        }
+                System.out.println("Enter the row you want to make your move in (start at 0)");
 
+               // if(input.nextInt() ){
+                    int rowInput = input.nextInt();
+                //}
+                
+                System.out.println("Enter the column you want to make your move in (start at 0)");
+                int colInput = input.nextInt();
+                
+                Tile move2 = new Tile();
+                move2.value = "O";
+                move2.row = rowInput;
+                move2.column = colInput;
+
+                boardObj.updateBoard(move2, GameType.TICTACTOE);
+                turn = 0;
+            } 
+       }
     }
 }
