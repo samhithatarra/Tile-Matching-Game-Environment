576838aad3e9c4bd0909c4d58751f468d4fbbfbc
aliyah-qt <aliyahclayton.f@gmail.com>
2022-03-05 20:35:09 -0800

    Added methods to check win and check if there is a full board.

--- src/Board.java
+++ src/Board.java
@@ -2,23 +2,29 @@ public class Board{
 
     // row, col
     Tile[][] boardArr;
+    static final int ROW = 3;
+    static final int COL = 3;
 
     public Board(){
-        boardArr = new Tile[8][8];
+        boardArr = new Tile[ROW][COL];
     }
 
+    // can also be used to reset board?
     public void createBoard(GameType gType){
-        if (gType ==  GameType.BATTLESHIP){
-            for(int row = 0; row < boardArr.length; row++){
-                for(int col = 0; col < boardArr[0].length; col++){
+
+        // BATTLESHIP        
+        // if(gType ==  GameType.BATTLESHIP){
+        //     for(int row = 0; row < boardArr.length; row++){
+        //         for(int col = 0; col < boardArr[0].length; col++){
     
-                    // boardArr[row][col].column = col;
-                    // boardArr[row][col].row = row;
-                    // boardArr[row][col].value = "0";
-                    
-                }
-            }
-        }
+        //             // boardArr[row][col].column = col;
+        //             // boardArr[row][col].row = row;
+        //             // boardArr[row][col].value = "0";                    
+        //         }
+        //     }
+        // }
+
+        // TIC TAC TOE
         if (gType ==  GameType.TICTACTOE){
             for(int row = 0; row < boardArr.length; row++){
                 for(int col = 0; col < boardArr[0].length; col++){
@@ -27,9 +33,7 @@ public class Board{
                     Tile tile = boardArr[row][col];
                     tile.column = col;
                     tile.row = row;
-                    tile.value = "0";
-
-
+                    tile.value = "_";
                 }
             }
         }
@@ -41,38 +45,107 @@ public class Board{
             String boardString = "";
 
             for(int row = 0; row < boardArr.length; row++){
-                boardString+="\n";
+
+                boardString += "\n";
+
                 for(int col = 0; col < boardArr[0].length; col++){
-                    boardString+=boardArr[row][col].value;
+                    boardString += boardArr[row][col].value;
                     boardString += " ";
-    
                 }
             }
             System.out.println(boardString);
         }
-        
-
     }
 
-    // Board updateBoard(){
+    public boolean checkWin(Tile[][] boardArr){
+        Tile tile = new Tile();
+        boolean win = false;
 
-        
-      
-        
-    // }
+        // check horizontals (row)
+        for(int row = 0; row < boardArr.length-1; row++){
 
-    public void resetBoard(GameType gType){
-        if(gType ==  GameType.TICTACTOE){
-            for(int row = 0; row < boardArr.length; row++){
-                for(int col = 0; col < boardArr[0].length; col++){
-                    // TO DO: clear space on board
+            // check adjacents are same starting with 1st col (index 0)
+            if(boardArr[row][0] == boardArr[row][1]){
+                if(boardArr[row][1] == boardArr[row][2]){
+                    tile.value = boardArr[row][0].toString();
+
+                    if(tile.value == "X"){
+                        win = true;
+                        System.out.println("Player 1 wins!");
+                    }
+                    else
+                    {
+                        win = true;
+                        System.out.println("Player 2 wins!");
+                    }
                 }
             }
         }
+
+        // check verticals (cols)
+        for(int col = 0; col < boardArr.length-1; col++){
+            // check adjacents are same starting with 1st row (index 0)
+            if(boardArr[0][col] == boardArr[1][col]){
+                if(boardArr[1][col] == boardArr[2][col]){
+                    tile.value = boardArr[0][col].toString();
+
+                    if(tile.value == "X"){
+                        win = true;
+                        System.out.println("Player 1 wins!");
+                    }
+                    else
+                    {
+                        win = true;
+                        System.out.println("Player 2 wins!");
+                    }
+                }
+            }
+        }
+
+        // check diagonals
+        // from top left to bottom right
+        if(boardArr[0][0] == boardArr[1][1]){
+            if(boardArr[1][1] == boardArr[2][2]){
+                tile.value = boardArr[0][0].toString();
+
+                if(tile.value == "X"){
+                    System.out.println("Player 1 wins!");
+                }
+                else
+                {
+                    System.out.println("Player 2 wins!");
+                }
+            }
+        }
+
+        // from bottom left to top right
+        if(boardArr[2][0] == boardArr[1][1]){
+            if(boardArr[1][1] == boardArr[0][2]){
+                tile.value = boardArr[2][0].toString();
+
+                if(tile.value == "X"){
+                    System.out.println("Player 1 wins!");
+                }
+                else
+                {
+                    System.out.println("Player 2 wins!");
+                }
+            }
+        }
+
+        return win;
     }
 
-    
+    // check if tie game
+    public boolean fullBoard(Tile[][] boardArr){
 
-
-
+        for(int row = 0; row < boardArr.length; row++) {
+			for(int col = 0; col < boardArr[0].length; col++) {
+				if(boardArr[row][col].toString() == "_") {
+					return false;
+				}
+			}
+		}        
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Board obj = new Board();
-        obj.createBoard(GameType.TICTACTOE);
-        obj.displayBoard(GameType.TICTACTOE);
+
+        // To test board
+        Board boardObj = new Board();
+        boolean gameFin = false;
+        int turn = 0;   // 0 reps player 1's turn, 1 reps palyer 2's turn
+
+        // User input
+        //Scanner input = new Scanner(System.in);
+
+
+        // TIC TAC TOE LOGIC
+        System.out.println("~ TIC TAC TOE ~");
+        System.out.println();
+        System.out.println("Player 1's game piece will be: X");
+		System.out.println("Player 2's game piece will be: O");
+
+        while(!gameFin){
+            boardObj.createBoard(GameType.TICTACTOE);
+            boardObj.displayBoard(GameType.TICTACTOE);
+
+            // player 1's turn
+            if(turn == 0){
+                System.out.println("Player 1's turn (X): ");
+            }
+            else{    // player 2's turn
+                System.out.println("Player 2's turn (O): ");
+            }
+        }
+
     }
 }
