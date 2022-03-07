ecb2f035976449d1338c3179168f535f0260d6c7
Jules Labador <juleslabador@gmail.com>
2022-03-07 13:29:37 -0800

    Deleted Game class

---
+++ src/ConnectFour.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ConnectFour {
+
+    ArrayList<String> players = new ArrayList<>();
+    Board board = null;
+    int playerTurn = 0;
+    Scanner input = new Scanner(System.in);
+
+    public ConnectFour() {
+
+    }
+
+    public boolean isValidInput() { return false; }
+
+    public void promptUserForPlayerProfiles() {}
+
+    public void updateBoard(int rowNumber, int colNumber, Tile tile) {
+        this.board.boardArr[rowNumber][colNumber] = tile;
+    }
+
+    public void createBoard(int rowCount, int columnCount) {
+        this.board = new Board(rowCount, columnCount);
+    }
+
+    public boolean checkWinner() { return false; }
+}
